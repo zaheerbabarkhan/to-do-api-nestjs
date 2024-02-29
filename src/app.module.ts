a00701ad0512ecaf117e3f69ce27e2fa73c85106
zaheerbabarkhan <zaheerbaberkhan@gmail.com>
2024-02-29 15:01:49 +0500

    cron added for the reminder email for pending todos

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,25 +9,28 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { RedisModule } from './modules/redis/redis.module';
 import * as nodemailer from "nodemailer";
 import { Schemas } from './schemas';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CronModule } from './modules/cron/cron.module';
 const smtpConfig = config.SMTP;
 @Module({
   imports: [
     MongooseModule.forRoot(`mongodb+srv://${config.DB.DB_USER}:${config.DB.DB_PASSWORD}@cluster0.xlw87.mongodb.net/?retryWrites=true&w=majority`, {
       dbName: config.DB.DB_NAME
-  }), MongooseModule.forFeature(Schemas),MailerModule.forRoot({
-    // transport: `smtps://${config.SMTP.SMTP_EMAIL}:${config.SMTP.SMTP_PASSWORD}@${config.SMTP.SMTP_HOST}`,
-    transport: nodemailer.createTransport({
-      host: smtpConfig.SMTP_HOST,
-      port: smtpConfig.SMTP_PORT,
-      auth: {
-          user: smtpConfig.SMTP_EMAIL,
-          pass: smtpConfig.SMTP_PASSWORD
+    }), MongooseModule.forFeature(Schemas), MailerModule.forRoot({
+      transport: `smtps://${config.SMTP.SMTP_EMAIL}:${config.SMTP.SMTP_PASSWORD}@${config.SMTP.SMTP_HOST}`,
+      //   transport: nodemailer.createTransport({
+      //     host: smtpConfig.SMTP_HOST,
+      //     port: smtpConfig.SMTP_PORT,
+      //     auth: {
+      //         user: smtpConfig.SMTP_EMAIL,
+      //         pass: smtpConfig.SMTP_PASSWORD
+      //     }
+      // }),
+      defaults: {
+        from: '"no-reply" <example@example.com>',
       }
-  }),
-    defaults: {
-      from: '"no-reply" <example@example.com>',
-    }}),JwtModule.register({
+    }), JwtModule.register({
       secret: config.JWT.SECRET_KEY,
-  }), RedisModule, UserModule, TodoModule, MailModule],
+    }), RedisModule, UserModule, TodoModule, MailModule, ScheduleModule.forRoot(), CronModule],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/modules/cron/cron.servicve.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { Model } from 'mongoose';
+import status from 'src/constants/status';
+import { User, UserDocument } from 'src/schemas/User.schema';
+import { MailService } from '../mail/mail.service';
+
+@Injectable()
+export class CronJobsService {
+    constructor(@InjectModel(User.name) private readonly User: Model<UserDocument>,
+    private readonly jwtService: JwtService,
+    private readonly mailService: MailService) { }
+
+    @Cron("0 0 * * *")
+    async openForBusiness() {
+        const todayDate = new Date();
+        todayDate.setUTCHours(0, 0, 0, 0);
+        const nextDay = new Date(todayDate);
+        nextDay.setDate(nextDay.getDate() + 1);
+        const pipeline = [
+            {
+                $match: {
+                    statusId: { $ne: status.DELETED }
+                }
+            },
+            {
+                $lookup: {
+                    from: "todos",
+                    localField: "_id",
+                    foreignField: "userId",
+                    as: "todos",
+                    pipeline: [
+                        {
+                            $match: {
+                                statusId: status.PENDING,
+                                dueDate: {
+                                    $gte: todayDate,
+                                    $lt: nextDay
+                                }
+                            }
+                        },
+                    ]
+                }
+            },
+        ];
+        const pendingTodosUsers = await this.User.aggregate(pipeline)
+        for (const user of pendingTodosUsers) {
+            if (user.todos.length) {
+                const token = this.jwtService.sign({
+                    userId: user._id
+                  });
+                  try {
+                    console.log("Sending reminder message to user: ", user.email);
+                    await this.mailService.reminderEmail(user.email, token, `&status_id=${status.PENDING}&dueDate=${new Date()}`)
+                  } catch (error) {
+                    console.log("error orccured while sending reminder email", error);
+                  }
+            }
+        }
+    }
+}

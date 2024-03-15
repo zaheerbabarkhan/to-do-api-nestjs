fc40215b23cce24d6ef4e58a6dc31eea7f2808eb
zaheerbabarkhan <zaheerbaberkhan@gmail.com>
2024-03-15 15:18:48 +0500

    forgot password functionality added

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -4,39 +4,55 @@ import config from '../../config/config';
 
 @Injectable()
 export class MailService {
-    constructor(private readonly mailerService: MailerService) {}
+    constructor(private readonly mailerService: MailerService) { }
 
     async confirmationEmail(to: string, token: string) {
         const confirmationLink = config.NODE_ENV === "development" ? `http://${config.HOST}:${config.PORT}/users/confirm-email?token=${token}` : `${config.HOST}/users/confirm-email?token=${token}`;
 
-    const emailContent = `
+        const emailContent = `
     <p>Please click on the following link to confirm your email:</p>
     <a href="${confirmationLink}">${confirmationLink}</a>
   `;
 
-    await this.mailerService.sendMail({
-        from: config.SMTP.SMTP_EMAIL, // sender address
-        to, // list of receivers
-        subject: "Confirm Your Email  ✔", // Subject line
-        text: `please click on the following link to confirm you email ${confirmationLink}`, // plain text body 
-        html: emailContent, // HTML version of the email
-    });
+        await this.mailerService.sendMail({
+            from: config.SMTP.SMTP_EMAIL, // sender address
+            to, // list of receivers
+            subject: "Confirm Your Email  ✔", // Subject line
+            text: `please click on the following link to confirm you email ${confirmationLink}`, // plain text body 
+            html: emailContent, // HTML version of the email
+        });
     }
 
-    async reminderEmail (to: string, token: string, queryParams: string) {
+    async reminderEmail(to: string, token: string, queryParams: string) {
         const dueToDos = config.NODE_ENV === "development" ? `http://${config.HOST}:${config.PORT}/todos?token=${token}` : `${config.HOST}/todos?token=${token}${queryParams}`;
 
-    const emailContent = `
+        const emailContent = `
     <p>Please click on the following link to see what needs to be completed today:</p>
     <a href="${dueToDos}">${"Due To-Dos"}</a>
   `;
 
-    await this.mailerService.sendMail({
-        from: config.SMTP.SMTP_EMAIL, // sender address
-        to, // list of receivers
-        subject: "To-Dos Due Today  ✔", // Subject line
-        text: `please click on the following link to see what needs to be completed today ${dueToDos}`, // plain text body 
-        html: emailContent, // HTML version of the email
-    });
+        await this.mailerService.sendMail({
+            from: config.SMTP.SMTP_EMAIL, // sender address
+            to, // list of receivers
+            subject: "To-Dos Due Today  ✔", // Subject line
+            text: `please click on the following link to see what needs to be completed today ${dueToDos}`, // plain text body 
+            html: emailContent, // HTML version of the email
+        });
+    }
+
+    async forgotPassword(to: string, token: string) {
+        const newPasswordLink = config.NODE_ENV === "development" ? `http://${config.HOST}:${config.PORT}/users/new-password?token=${token}` : `${config.HOST}/users/new-password?token=${token}`;
+        const emailContent = `
+    <p>Please click on the following link to create new password:</p>
+    <a href="${newPasswordLink}">${"Create new password"}</a>
+  `;
+
+        await this.mailerService.sendMail({
+            from: config.SMTP.SMTP_EMAIL, // sender address
+            to, // list of receivers
+            subject: "Create New Password  ✔", // Subject line
+            text: `please click on the following link to create new password ${newPasswordLink}`, // plain text body 
+            html: emailContent, // HTML version of the email
+        });
     }
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Query, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, Request, UseGuards, Req, HttpStatus, HttpException } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserLoginDTO } from './dto/user-login.dto';
@@ -27,12 +27,21 @@ export class UserController {
     return this.userService.confirmEmail(token);
   }
 
+  @Post("forgot-password")
+  forgotPassword(@Request() req) {
+    if (!req || !req.body || !req.body.email) {
+      throw new HttpException("Invalid credentials1", HttpStatus.BAD_REQUEST)
+    }
+    return this.userService.forgotPassword(req.body.email)
+  }
+
   @UseGuards(AuthGuard)
   @Post("logout")
   logout(@Request() req){
     const token = req.headers.authorization.split(" ")[1];
     return this.userService.logout(req.user._id, token);
   }
+
   
   @Get("google")
   @UseGuards(GoogleOAuthGuard)
